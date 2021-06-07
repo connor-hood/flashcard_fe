7b4c4043663674b3b61d9c2846cf4599acb387e4
Connor Hood <connor_hood@yahoo.com>
2021-06-07 11:34:44 -0500

    added card viewer and language selection components, working on selection page now

---
+++ src/components/CardViewer/cardViewer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CardViewer(props){
+    return(
+        <div>
+            <p>life</p>
+        </div>
+    )
+}
+
+export default CardViewer;
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,28 +4,36 @@ import './app.css'
 
 
 class App extends Component {
-    state = { }
+    state = { 
+        cardCount: 0
+    }
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
                 <button className="jsbutton">
                     <h2>JS</h2>
+                    <h4>{this.state.cardCount}</h4>
                 </button>
                 <button className="htcsbutton">
                     <h2>HTML/CSS</h2>
+                    <h4>{this.state.cardCount}</h4>
                 </button>
                 <button className="pybutton">
                     <h2>Python</h2>
+                    <h4>{this.state.cardCount}</h4>
                 </button>
                 <button className="sqlbutton">
                     <h2>SQL</h2>
+                    <h4>{this.state.cardCount}</h4>
                 </button>
                 <button className="djangobutton">
                     <h2>Django</h2>
+                    <h4>{this.state.cardCount}</h4>
                 </button>
                 <button className="reactbutton">
                     <h2>React</h2>
+                    <h4>{this.state.cardCount}</h4>
                 </button>
             </div>
         );
