8e130315a8789063c383f26b89e080d85b9c3047
Connor Hood <connor_hood@yahoo.com>
2021-06-07 10:29:25 -0500

    added other buttons with proper css codes for language colors

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -12,6 +12,21 @@ class App extends Component {
                 <button className="jsbutton">
                     <h1>JS</h1>
                 </button>
+                <button className="htcsbutton">
+                    <h1>HTML/CSS</h1>
+                </button>
+                <button className="pybutton">
+                    <h1>Python</h1>
+                </button>
+                <button className="sqlbutton">
+                    <h1>SQL</h1>
+                </button>
+                <button className="djangobutton">
+                    <h1>Django</h1>
+                </button>
+                <button className="reactbutton">
+                    <h1>React</h1>
+                </button>
             </div>
         );
     }
