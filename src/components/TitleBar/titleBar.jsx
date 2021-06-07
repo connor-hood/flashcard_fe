9f0c7f97c030b805387151ddebde9f880ed82253
Connor Hood <connor_hood@yahoo.com>
2021-06-07 10:20:58 -0500

    got JS button added, adding rest of language buttons with proper styling

--- src/components/TitleBar/titleBar.jsx
+++ src/components/TitleBar/titleBar.jsx
@@ -3,7 +3,7 @@ import './titleBar.css';
 
 function TitleBar(props) {
     return (
-        <div className="row justify-content-around">
+        <div className="row justify-content-center">
             <div className="col-md-4" style={{paddingTop: 65}}>
                 <div className="titlebar-nav">
                     <h1>Coding Flashcards</h1>
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import TitleBar from './TitleBar/titleBar'
+import './app.css'
 
 
 class App extends Component {
@@ -8,6 +9,9 @@ class App extends Component {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <button className="jsbutton">
+                    <h1>JS</h1>
+                </button>
             </div>
         );
     }
